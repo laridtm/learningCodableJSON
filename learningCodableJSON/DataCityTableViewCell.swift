20a858d0aca0a3a387043d6d6f11ae96a5067d9f
Larissa <>
2020-02-11 16:25:23 -0300

    create Outlets to second screen

---
+++ learningCodableJSON/DataCityTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DataCityTableViewCell.swift
+//  learningCodableJSON
+//
+//  Created by Larissa Diniz  on 11/02/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import UIKit
+
+class DataCityTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var nameCityLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var valueHumidityLabel: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var valueMinLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var valueMaxLabel: UILabel!
+    
+//    func setLabels(city: City) {
+//        nameCityLabel.text =
+//        valueHumidityLabel.text =
+//        valueMinLabel.text =
+//        valueMaxLabel.text =
+//    }
+}
