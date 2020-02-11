08f3db1b4c4b15f61808be4bcfcef39e43e85ac5
Larissa <>
2020-02-11 13:54:13 -0300

    add func didSelect

--- learningCodableJSON/ViewController.swift
+++ learningCodableJSON/ViewController.swift
@@ -36,6 +36,8 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
