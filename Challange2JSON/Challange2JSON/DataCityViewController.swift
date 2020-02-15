95a7416c3204d99d4cd81b54246bfad1992137a6
Larissa <>
2020-02-14 23:15:47 -0300

    create second screen

---
+++ Challange2JSON/Challange2JSON/DataCityViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DataCityViewController.swift
+//  Challange2JSON
+//
+//  Created by Larissa Diniz  on 14/02/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import UIKit
+
+class DataCityViewController: UIViewController {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var climateLabel: UILabel!
+    @IBOutlet weak var tempMaxLabel: UILabel!
+    @IBOutlet weak var tempMinLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
