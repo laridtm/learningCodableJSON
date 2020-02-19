190775976027fb6ab6956577623fe4ff698c927e
Larissa <>
2020-02-19 17:49:39 -0300

    removed ifs

--- Challange2JSON/Challange2JSON/DataCityViewController.swift
+++ Challange2JSON/Challange2JSON/DataCityViewController.swift
@@ -23,23 +23,22 @@ class DataCityViewController: UIViewController {
     let session = URLSession.shared
     
     var currentURL: URL?
-       
-    let urlRainy = URL(string: "https://centralca.cdn-anvilcms.net/media/images/2019/01/02/images/Rainy_Weather_pix.max-752x423.jpg")!
-    let urlSunny = URL(string: "https://www.breakingbelizenews.com/wp-content/uploads/2019/06/Warm-weather.jpg")!
-    let urlCloudy = URL(string: "https://hoodline.imgix.net/uploads/story/image/402715/istock__..featured_image_1..cloudy_2.jpg.jpg?auto=format")!
+    let weatherImageUrl = ["rainy": "https://centralca.cdn-anvilcms.net/media/images/2019/01/02/images/Rainy_Weather_pix.max-752x423.jpg",
+                           "cloudy": "https://hoodline.imgix.net/uploads/story/image/402715/istock__..featured_image_1..cloudy_2.jpg.jpg?auto=format",
+                           "sunny": "https://www.breakingbelizenews.com/wp-content/uploads/2019/06/Warm-weather.jpg"]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if selectedCity?.climate == "rainy" {
-            currentURL = urlRainy
-        } else if selectedCity?.climate == "cloudy" {
-            currentURL = urlCloudy
-        } else if selectedCity?.climate == "sunny" {
-            currentURL = urlSunny
+        guard let currentCity = selectedCity else {
+            return
         }
         
-        let task = self.session.dataTask(with: self.currentURL!) { data, response, error in
+        guard let url = URL(string: weatherImageUrl[currentCity.climate]!) else {
+            fatalError()
+        }
+        
+        let task = self.session.dataTask(with: url) { data, response, error in
             if error == nil {
                 let loadImage = UIImage(data: data!)
                 DispatchQueue.main.async {
@@ -50,10 +49,6 @@ class DataCityViewController: UIViewController {
         task.resume()
         
         
-        guard let currentCity = selectedCity else {
-            return
-        }
-
         cityLabel.text = name
         humidityLabel.text = "\(currentCity.humidity)%"
         climateLabel.text = currentCity.climate
@@ -63,5 +58,5 @@ class DataCityViewController: UIViewController {
         
     }
     
-
+    
 }
